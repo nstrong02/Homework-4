c35ec6ae88064120c67696e7ca5adbdf19c4d2d5
nstrong02 <45565394+nstrong02@users.noreply.github.com>
2018-12-10 08:12:15 -0600

    Add files via upload

---
+++ 22_hmwk4.py
@@ -0,0 +1,65 @@
+msg = "Python is fun."
+print(msg)
+#2.1
+
+
+msg = "Python is fun."
+print(msg)
+
+msg = "There is much to learn"
+print(msg)
+#2.2
+
+
+name = "Donny"
+print("Hello, " + name.title() + "!")
+#2.3
+
+
+name = "Donny"
+print(name.upper())
+print(name.lower())
+print(name.title())
+#2.4
+
+
+print('Kanye once said, "I have robot eyes,')
+print(' I will <explative> laser you."')
+#2.5
+
+
+famous_person = "Kanye"
+message = " once said, I have robot eyes, I will <explative> laser you."
+
+print(famous_person.title() + message.title())
+#2.6
+
+
+name = "\tDonny Jimmins\n"
+
+print("Unmodified:")
+print(name)
+
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+print("\nUsing rstrip():")
+print(name.rstrip())
+
+print("\nUsing strip():")
+print(name.strip())
+#2.7
+
+
+print(16 / 2)
+print(4 * 2)
+print(7 + 1)
+print(9 - 1)
+#2.8
+
+
+fav_num = 5
+msg = "My favorite number is " + str(fav_num) + "."
+
+print(msg)
+#2.8
